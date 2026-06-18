c2bfc30ad432801b8bba9bb80d61aa6f65fa7ee7
PradanaAkbarRazan <pradana.akbar.razan@gmail.com>
2026-06-18 17:38:39 +0700

    epic1 : Otentikasi & Manajemen Menu Berbasis Role

---
+++ app/src/main/java/SistemRentalKendaraan/presentation/StaffDashboard.java
@@ -0,0 +1,12 @@
+package SistemRentalKendaraan.presentation;
+
+public class StaffDashboard {
+    public void showMenu() {
+        ConsoleHelper.printHeader("DASHBOARD STAFF");
+        System.out.println("1. Transaksi Peminjaman");
+        System.out.println("2. Transaksi Pengembalian");
+        System.out.println("3. Kelola Pelanggan");
+        System.out.println("0. Logout");
+        ConsoleHelper.getInput("Pilih menu: ");
+    }
+}
